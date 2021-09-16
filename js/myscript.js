1e2742ef3baaf5ef01067eeec55319716966fd24
josecoki1 <josecoki@byui.edu>
2021-09-16 16:28:41 -0600

    Updating Home Page

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+let d = new Date();
+
+let year = d.getFullYear();
+
+document.getElementById("currentyear").textContent = year;
+
+/*let oLastModif = new Date(document.lastModified);*/
+
+document.getElementById("lastUpdated").textContent = document.lastModified;
