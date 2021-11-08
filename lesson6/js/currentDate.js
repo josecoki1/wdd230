b5756e066c1b9fbc207e64dc8c76bc830bafa024
Jose F Hernandez <59710223+josecoki1@users.noreply.github.com>
2021-11-08 09:32:47 -0700

    update currentdate.js

--- lesson6/js/currentDate.js
+++ lesson6/js/currentDate.js
@@ -7,5 +7,5 @@ console.log(event.toLocaleDateString(undefined, options));
 function getCurrentDate() {
     return event.toLocaleDateString('en-UK',options);
 };
-
+//
 document.getElementById("currentDateString").textContent = getCurrentDate();
