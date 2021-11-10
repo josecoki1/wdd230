3508966459226559def0198170f9189716c997cf
Jose F Hernandez <59710223+josecoki1@users.noreply.github.com>
2021-11-10 09:51:50 -0700

    json assignment

--- lesson9/prophets/js/prophets.js
+++ lesson9/prophets/js/prophets.js
@@ -7,6 +7,27 @@ fetch(requestURL)
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
     const prophets = jsonObject['prophets'];
-  });
-
+    for (let i = 0; i < prophets.length; i++ ) {
+  
+  let card = document.createElement('section');
+  let h2 = document.createElement('h2');
+  let bd = document.createElement('p');
+  let bp = document.createElement('p');
+  let img = document.createElement('img');
+  
+  h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+  bd.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+  bp.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+img.setAttribute('src', prophets[i].imageurl);
+img.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order);
+  
+  card.appendChild(h2);
+  card.appendChild(bd);
+  card.appendChild(bp);
+  card.appendChild(img);
+  
+  document.querySelector('div.cards').appendChild(card);
  
+
+  }});
+
