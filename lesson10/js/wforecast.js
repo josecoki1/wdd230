23fcb838966812233551e576acc181a1c3e5f6af
Jose F Hernandez <59710223+josecoki1@users.noreply.github.com>
2021-11-22 16:48:31 -0700

    Fixed forecast wk10

--- lesson10/js/wforecast.js
+++ lesson10/js/wforecast.js
@@ -1,4 +1,4 @@
-const requestforecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=8f46d005ff9b2c64a98df419a1a881dd";
+const requestforecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=8f46d005ff9b2c64a98df419a1a881dd";
 
 fetch(requestforecastURL)
   .then((response) => response.json())
@@ -15,6 +15,20 @@ fetch(requestforecastURL)
   var results = forecast1.filter(({ dt_txt} ) => dt_txt.includes('18:00:00'));
 console.log(results);
 
+const daysofweek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+let day = 0;
+
+results.forEach(weaforecast => {
+  let eventdate = new Date(weaforecast.dt_txt);
+  let forcasttemp = weaforecast.main.temp
+  document.querySelector(`#forcastdayvalue${day + 1}`).textContent = daysofweek[eventdate.getDay()];
+  document.querySelector(`#forcasttempvalue${day + 1}`).textContent = forcasttemp.toFixed(0);
+  day++;
+});
+
+});
+
+/*
 const weatherforecast = results['main'];
    for (let i = 0; i < weatherforecast.temp; i++ ) {
 
@@ -28,7 +42,4 @@ const weatherforecast = results['main'];
     document.querySelector('tr.forecast-results').appendChild(foretempval);
   }
 
-});
-
-
-  
+*/
