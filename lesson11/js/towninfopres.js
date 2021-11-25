5b48845ab1db741fef2d5d1b49c17db735121b9c
Jose F Hernandez <59710223+josecoki1@users.noreply.github.com>
2021-11-25 11:49:31 -0700

    Update week 11

---
+++ lesson11/js/towninfopres.js
@@ -0,0 +1,33 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    //console.log(jsonObject);
+
+   
+    let towninfo = jsonObject['towns'];
+   towninfo.forEach((eventitem) => {
+     if (eventitem.name == "Preston"){
+  
+  let eventsdatasection = document.createElement('section');
+   let events1 = document.createElement('p');
+  let events2 = document.createElement('p');
+  let events3 = document.createElement('p');
+
+ 
+
+  events1.innerHTML = eventitem.events[0];
+  events2.innerHTML = eventitem.events[1];
+  events3.innerHTML = eventitem.events[2];
+
+  eventsdatasection.appendChild(events1);
+  eventsdatasection.appendChild(events2);
+  eventsdatasection.appendChild(events3);
+
+  document.querySelector('div.upevents').appendChild(eventsdatasection);
+   }
+   })
+  });
